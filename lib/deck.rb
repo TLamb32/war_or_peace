0d599686376ca0c63ec9f6e62224fa8ce27e4bf6
Terry Lamb <terrylamb@gmail.com>
2023-07-05 16:10:06 -0400

    add #rank_of_card_at method and accompanying test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,17 @@
 class Deck
+  attr_reader :cards
 
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(index)
+    if cards[index].nil?
+      0
+    else
+      @cards[index].rank
+    end
+  end
+
+  
 end
