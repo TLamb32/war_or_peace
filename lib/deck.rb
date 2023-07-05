a7a98bdf343a33822f2f0e1683929307bfc5563a
Terry Lamb <terrylamb@gmail.com>
2023-07-05 16:35:56 -0400

    add #high_ranking_cards and #percent_high_ranking methods and tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -13,5 +13,13 @@ class Deck
     end
   end
 
-  
+  def high_ranking_cards
+    @cards.find_all do |card|
+      card.rank > 10
+    end
+  end
+
+  def percent_high_ranking
+    (high_ranking_cards.count.to_f / cards.count.to_f).round(4) * 100
+  end
 end
